d21bca424393f386d309ca5ef1be26bbdc65dc9f
RomAFCI <adnot.rom@gmail.com>
2025-07-22 16:27:45 +0200

    save

--- indexadmin.php
+++ indexadmin.php
@@ -9,6 +9,11 @@ $stmtGenre = $pdo->prepare($sqlGenre);
 $stmtGenre->execute();
 $resultGenre = $stmtGenre->fetchAll(PDO::FETCH_ASSOC);
 
+$sqlAllLivre = "SELECT * FROM `livres`";
+$stmtAllLivre = $pdo->prepare($sqlAllLivre);
+$stmtAllLivre->execute();
+$resultsAllLivre = $stmtAllLivre->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +25,8 @@ $resultGenre = $stmtGenre->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <body>
+    <hr>
+    <h2>Ajout</h2>
     <h3>Ajout d'écrivain</h3>
     <a href="?page=AddEcrivain">
         <p>Ajoutez un écrivain</p>
@@ -162,10 +169,79 @@ $resultGenre = $stmtGenre->fetchAll(PDO::FETCH_ASSOC);
 
         $stmtAddLivre->execute();
     }
-
-
-
     // FIN DE L'ENVOI POUR LE LIVRE
+    ?>
+
+<hr>
+<h2>Modification</h2>
+<h3>Modifcation des livres</h3>
+
+
+<?php
+// include dans indexModif ??? ⚠️
+// MODIFICATION DES LIVRES EN BDD
+    foreach ($resultsAllLivre as $key => $value) {
+        $idModifier = $value['idLivre'];
+        foreach ($value as $key => $value2) {
+            echo htmlspecialchars($key) . " : " . htmlspecialchars($value2);
+            echo "<br>";
+        }
+        echo '<a href="?id=' . $idModifier . '">Modifier</a>';
+        echo "<br>";
+        echo "<br>";
+    }
+?>
+
+<hr> 
+
+    <?php
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $sqlId = "SELECT * FROM `users` WHERE `idUser` = '$id'";
+        $stmtId = $pdo->prepare($sqlId);
+        $stmtId->execute();
+        $resultsId = $stmtId->fetchAll(PDO::FETCH_ASSOC);
+
+        echo ' <form method="POST">
+        <input type="hidden" name="idUpdate" value="' . htmlspecialchars($resultsId[0]['idUser']) . '">
+        <br>
+        <label>nom</label>
+        <input type="text" name="updateNom" value="' . htmlspecialchars($resultsId[0]['nomUser']) . '">
+        <br>
+        <label>prenom</label>
+        <input type="text" name="updatePrenom" value="' . htmlspecialchars($resultsId[0]['prenomUser']) . '">
+        <br>
+        <label>age</label>
+        <input type="text" name="updateAge" value="' . htmlspecialchars($resultsId[0]['ageUser']) . '">
+        <br>
+        <label>adresse mail</label>
+        <input type="text" name="updateAdresseMail" value="' . htmlspecialchars($resultsId[0]['adresseMailUser']) . '">
+        <br>
+        <label>mot de passe</label>
+        <input type="text" name="updatePassword" value="' . htmlspecialchars($resultsId[0]['passwordUser']) . '">
+        <br>
+        <input type="submit" name="submitUpdate" value="Mettre à jour les données">
+    </form>';
+    }
+
+    if (isset($_POST['submitUpdate'])) {
+
+        $idUpdate = $_POST['idUpdate'];
+        $nom = $_POST['updateNom'];
+        $prenom = $_POST['updatePrenom'];
+        $age = $_POST['updateAge'];
+        $adresseMail = $_POST['updateAdresseMail'];
+        $password = $_POST['updatePassword'];
+
+        $hashPassword = password_hash('$password', PASSWORD_DEFAULT);
+
+        $sqlUpdate = "UPDATE `users` SET `nomUser`='$nom',`prenomUser`='$prenom',`ageUser`='$age',`adresseMailUser`='$adresseMail',`passwordUser`='$hashPassword' WHERE idUser='$idUpdate'";
+        $stmtUpdate = $pdo->prepare($sqlUpdate);
+        $stmtUpdate->execute();
+
+        header("Location: logUser.php");
+    }
     ?>
 
 </body>
